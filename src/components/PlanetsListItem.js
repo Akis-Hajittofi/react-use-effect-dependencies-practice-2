23706368aceaf3bcf29e269cfdb95f3d4d9cb775
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-01-31 20:41:15 +0000

    Done

--- src/components/PlanetsListItem.js
+++ src/components/PlanetsListItem.js
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PlanetsListItem(props) {
   const { planet } = props;
 
-  const [firstFilm, setFirstFilm] = useState('')
+  const [firstFilm, setFirstFilm] = useState("");
 
   console.log("Planet", planet);
 
-  return <li>{planet.name} - First Film {firstFilm}</li>;
+  useEffect(() => {
+    fetch(planet.films[0])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("film data?", data);
+        setFirstFilm(data.title);
+      });
+  }, [planet]);
+
+  return (
+    <li>
+      {planet.name} - First Film {firstFilm}
+    </li>
+  );
 }
 
 export default PlanetsListItem;
--- src/components/StarshipsListItem.js
+++ src/components/StarshipsListItem.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function StarshipsListItem(props) {
   const { starship } = props;
 
-  const [firstPilot, setFirstPilot] = useState('')
+  const [firstPilot, setFirstPilot] = useState("");
 
   console.log({ starship });
 
-  return <li>{starship.name} - First Pilot {firstPilot}</li>;
+  useEffect(() => {
+    if (starship.pilots.length === 0) {
+      setFirstPilot("no pilots");
+    } else {
+      fetch(starship.pilots[0])
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("pilot data?", data);
+          setFirstPilot(data.name);
+        });
+    }
+  }, [starship]);
+
+  return (
+    <li>
+      {starship.name} - First Pilot {firstPilot}
+    </li>
+  );
 }
 
 export default StarshipsListItem;
